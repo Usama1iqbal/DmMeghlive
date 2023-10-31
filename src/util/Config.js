f585c27be23496325713b8e9498648210aa619b1
“Usama1iqbal” <usamaiqbal117@gmail.com>
2023-10-31 14:47:03 +0500

    base

--- src/util/Config.js
+++ src/util/Config.js
@@ -1,2 +1,2 @@
-exports.baseURL = "http://localhost:5000/"; // your base URL same as backend code
+exports.baseURL = "http://3.110.209.215/api/"; // your base URL same as backend code
 exports.key = "FdsvDYdvnX4rddvd9I"; // your secret key same as backend code
